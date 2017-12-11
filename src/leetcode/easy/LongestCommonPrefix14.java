8734ec8174e21dbf059572f77830ff549627ca0c
Manas Mukherjee <mmanas19@gmail.com>
2017-12-11 02:34:55 -0800

    Leetcode ( top-interview-qts easy. Ref - 1,7,13,14,20 )

--- src/leetcode/easy/LongestCommonPrefix14.java
+++ src/leetcode/easy/LongestCommonPrefix14.java
@@ -1,5 +1,16 @@
 package leetcode.easy;
 
+/*
+
+Complexity Analysis
+
+Time complexity : O(S)O(S) , where S is the sum of all characters in all strings.
+In the worst case there will be nn equal strings with length mm and the algorithm performs S = m*nS=m∗n character comparisons.
+Even though the worst case is still the same as Approach #1, in the best case there are at most n*minLenn∗minLen comparisons where minLenminLen is the length of the shortest string in the array.
+
+Space complexity : O(1)O(1). We only used constant extra space
+*/
+
 public class LongestCommonPrefix14 {
     public static String longestCommonPrefix(String[] input) {
         if(input==null || input.length==0){
